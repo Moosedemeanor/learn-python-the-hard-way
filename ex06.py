4b3b9c055fde4a53faf382b9cbb305aa5b309c62
RedPen Security <charles.bouley@redpensecurity.com>
2019-11-26 05:30:25 -0500

    Exercise 7, 8, 9

--- ex06.py
+++ ex06.py
@@ -5,3 +5,16 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 
 print x
 print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right ride."
+
+print w + e
---
+++ ex09.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "\nMon\nTues\nWed\nThu\nFri\nSat\nSun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the motnhs: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
